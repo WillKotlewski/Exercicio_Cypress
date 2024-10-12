3a3899eb66edaeeb45299ade17ff6134c6264307
Willian <willkotlewski@gmail.com>
2024-10-11 21:04:28 -0300

    exercicio concluido

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,14 +9,14 @@ Cypress.Commands.add('login', () => {
 
 Cypress.Commands.add('finalizarCheckout', () => {
     cy.visit('/checkout/')
-            cy.get('#billing_company').clear().type('Will.QA')
-            cy.get('#billing_address_1').clear().type('Rua 500, N° 500')
-            cy.get('#billing_address_2').clear().type('Ap 500')
-            cy.get('#billing_city').clear().type('São Paulo')
-            cy.get('#billing_postcode').clear().type('12345-123')
-            cy.get('#billing_phone').clear().type('55999999999')
-            cy.get('#order_comments').type('Entrega tudo certinho aí obg')
-            cy.get('#terms').click()
-            cy.get('#place_order').click()
+    cy.get('#billing_company').clear().type('Will.QA')
+    cy.get('#billing_address_1').clear().type('Rua 500, N° 500')
+    cy.get('#billing_address_2').clear().type('Ap 500')
+    cy.get('#billing_city').clear().type('São Paulo')
+    cy.get('#billing_postcode').clear().type('12345-123')
+    cy.get('#billing_phone').clear().type('55999999999')
+    cy.get('#order_comments').type('Entrega tudo certinho aí obg')
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
 })
 
